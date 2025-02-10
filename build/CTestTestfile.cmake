fc09a34e49ec786e856d6bbf4447c9560705abf5
kellyyuan333 <kellyyuan333@gmail.com>
2025-02-10 18:55:50 -0500

    removed files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kelly/Projects/bitbirch/development-code/bitbirchX
-# Build directory: C:/Users/Kelly/Projects/bitbirch/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
